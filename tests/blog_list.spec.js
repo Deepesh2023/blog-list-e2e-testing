c067db70de9379a77779423b427560c0c695779b
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-06-18 05:26:46 +0530

    Blog List End To End Testing, step 2, refactor

--- tests/blog_list.spec.js
+++ tests/blog_list.spec.js
@@ -1,25 +1,55 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 const baseUrl = 'http://localhost:5173'
 
-beforeEach(async ({ page, request }) => {
-  await request.post(`${baseUrl}/api/testing/reset`)
-  await request.post(`${baseUrl}/api/users`, {
-    data: {
-      name: 'deepesh',
-      username: 'deep_learning_42',
-      password: 'deepesh',
-    },
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post(`${baseUrl}/api/testing/reset`)
+    await request.post(`${baseUrl}/api/users`, {
+      data: {
+        name: 'deepesh',
+        username: 'deep_learning_42',
+        password: 'deepesh',
+      },
+    })
+
+    await page.goto(baseUrl)
   })
 
-  await page.goto(baseUrl)
-})
-
-describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     const usernameInput = await page.locator('#username')
     const passwordInput = await page.locator('#password')
 
     await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
     expect(usernameInput, passwordInput).toBeDefined()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.locator('#username').fill('deep_learning_42')
+      await page.locator('#password').fill('deepesh')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(
+        page.getByText('deep_learning_42 is logged in')
+      ).toBeVisible()
+
+      await expect(page.getByText('blogs')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.locator('#username').fill('deep_learning_42')
+      await page.locator('#password').fill('wrong password')
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.locator('.notification')).toHaveText(
+        'incorrect password'
+      )
+
+      await expect(
+        page.getByText('deep_learning_42 is logged in')
+      ).not.toBeVisible()
+
+      await expect(page.getByText('blogs')).not.toBeVisible()
+    })
   })
 })
