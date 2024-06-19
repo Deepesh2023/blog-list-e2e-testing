bac2ccb84f37cb056d2ed02be16c17ab8d2d836c
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-06-19 11:15:12 +0530

    Blog List End To End Testing, step 7

--- tests/blog_list.spec.js
+++ tests/blog_list.spec.js
@@ -1,13 +1,12 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 const testHelper = require('./test_helper')
 const exp = require('constants')
-const baseUrl = 'http://localhost:5173'
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post(`${baseUrl}/api/testing/reset`)
+    await request.post(`/api/testing/reset`)
 
-    await request.post(`${baseUrl}/api/users`, {
+    await request.post(`/api/users`, {
       data: {
         name: 'deepesh',
         username: 'deep_learning_42',
@@ -15,7 +14,7 @@ describe('Blog app', () => {
       },
     })
 
-    await request.post(`${baseUrl}/api/users`, {
+    await request.post(`/api/users`, {
       data: {
         name: 'binil',
         username: 'binil_balu',
@@ -23,7 +22,7 @@ describe('Blog app', () => {
       },
     })
 
-    await page.goto(baseUrl)
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
@@ -44,7 +43,7 @@ describe('Blog app', () => {
         page.getByText('deep_learning_42 is logged in')
       ).toBeVisible()
 
-      await expect(page.getByText('blogs')).toBeVisible()
+      await expect(page.getByRole('heading', { name: 'blogs' })).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
@@ -65,9 +64,11 @@ describe('Blog app', () => {
   })
 
   describe('When logged in', () => {
-    test('a new blog can be created', async ({ page }) => {
+    beforeEach(async ({ page }) => {
       await testHelper.logUserIn(page, 'deep_learning_42', 'deepesh')
+    })
 
+    test('a new blog can be created', async ({ page }) => {
       await page.getByRole('button', { name: 'New Blog' }).click()
       await page.locator('#title').fill('a playwright blog test')
       await page.locator('#author').fill('myself')
@@ -80,8 +81,12 @@ describe('Blog app', () => {
     })
 
     test('can like a blog', async ({ page }) => {
-      await testHelper.logUserIn(page, 'deep_learning_42', 'deepesh')
-      await testHelper.createBlog(page)
+      await testHelper.createBlog(
+        page,
+        'another helper blog',
+        'myself',
+        'someurl.com'
+      )
       await page.getByRole('button', { name: 'View' }).click()
       await page.getByRole('button', { name: 'Like' }).click()
 
@@ -89,8 +94,12 @@ describe('Blog app', () => {
     })
 
     test('can delete a blog', async ({ page }) => {
-      await testHelper.logUserIn(page, 'deep_learning_42', 'deepesh')
-      await testHelper.createBlog(page)
+      await testHelper.createBlog(
+        page,
+        'another helper blog',
+        'myself',
+        'someurl.com'
+      )
       await page.on('dialog', (dialog) => dialog.accept())
       await page.getByRole('button', { name: 'Delete' }).click()
 
@@ -102,8 +111,12 @@ describe('Blog app', () => {
     test('only the user added the blog can see the delete button', async ({
       page,
     }) => {
-      await testHelper.logUserIn(page, 'deep_learning_42', 'deepesh')
-      await testHelper.createBlog(page)
+      await testHelper.createBlog(
+        page,
+        'another helper blog',
+        'myself',
+        'someurl.com'
+      )
 
       const blogDiv = await page.locator('.blog')
       await expect(blogDiv).toContainText('another helper blog, myself')
@@ -116,6 +129,81 @@ describe('Blog app', () => {
       await expect(
         page.getByRole('button', { name: 'Delete' })
       ).not.toBeVisible()
+    })
+
+    test('blogs are arrenged in decending order based on likes', async ({
+      page,
+    }) => {
+      let likeButton = null
+      let blog = null
+
+      await testHelper.createBlog(
+        page,
+        'test blog with 0 likes',
+        'myself',
+        'tesurl.moc'
+      )
+
+      await testHelper.createBlog(
+        page,
+        'test blog with 5 likes',
+        'myself',
+        'tesurl.moc'
+      )
+
+      await testHelper.createBlog(
+        page,
+        'test blog with 10 likes',
+        'myself',
+        'tesurl.moc'
+      )
+
+      // gives he first blog 0 likes
+
+      // giving the second blog 5 likes
+      blog = await page
+        .locator('.blog')
+        .filter({ hasText: 'test blog with 5 likes' })
+
+      await blog.getByRole('button', { name: 'View' }).click()
+      likeButton = await blog.getByRole('button', { name: 'Like' })
+
+      for (let i = 0; i < 5; i++) {
+        await likeButton.click()
+      }
+
+      await expect(blog.getByText('Likes: 5')).toBeVisible()
+      await blog.locator('.hideButton').click()
+
+      // giving the third blog 10 likes
+      blog = await page
+        .locator('.blog')
+        .filter({ hasText: 'test blog with 10 likes' })
+
+      await blog.getByRole('button', { name: 'View' }).click()
+      likeButton = await blog.getByRole('button', { name: 'Like' })
+
+      for (let i = 0; i < 10; i++) {
+        await likeButton.click()
+      }
+
+      await expect(blog.getByText('Likes: 10')).toBeVisible()
+      await blog.locator('.hideButton').click()
+
+      await page.reload()
+      await expect(page.locator('.blog')).toHaveCount(3)
+
+      // getting the blogs list after page refresh
+      let blogs = await page.locator('.blog').all()
+
+      await blogs[0].getByText('View').click()
+      await expect(blogs[0]).toContainText('Likes: 10')
+
+      await blogs[1].getByText('View').click()
+      await expect(blogs[1]).toContainText('Likes: 5')
+
+      await blogs[2].getByText('View').click()
+      await expect(blogs[2]).toContainText('Likes: 0')
     })
   })
 })
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -4,12 +4,13 @@ const logUserIn = async (page, username, password) => {
   await page.getByRole('button', { name: 'Login' }).click()
 }
 
-const createBlog = async (page) => {
+const createBlog = async (page, title, author, url) => {
   await page.getByRole('button', { name: 'New Blog' }).click()
-  await page.locator('#title').fill('another helper blog')
-  await page.locator('#author').fill('myself')
-  await page.locator('#url').fill('someanotherurl.com')
+  await page.locator('#title').fill(title)
+  await page.locator('#author').fill(author)
+  await page.locator('#url').fill(url)
   await page.getByRole('button', { name: 'Add blog' }).click()
+  await page.getByText(title).waitFor()
 }
 
 module.exports = {
